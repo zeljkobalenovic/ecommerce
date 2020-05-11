145306dc4b3c367f903b8550d8aff5a91ed04eec
zeljkobalenovic <zeljkobalenovic@gmail.com>
2020-05-11 10:49:14 +0200

    product-list-grid + online shop templates

--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -4,13 +4,14 @@ import { Product } from 'src/app/common/product';
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
+  // templateUrl: './product-list.component.html',
+  templateUrl: './product-list-grid.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
 
   products : Product [];
-
+  
   constructor(private productService : ProductService) { }
 
   ngOnInit() {
@@ -18,9 +19,11 @@ export class ProductListComponent implements OnInit {
   }
 
   listProducts() {
+    let product1 = new Product();
     this.productService.getProductList().subscribe(
-      data => this.products=data
+      data => this.products=data        
     );
   }
 
 }
+
